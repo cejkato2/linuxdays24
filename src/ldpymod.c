733cea1a23e898e3b1be51827b97358286650fb9
Tomas Cejka <cejkat@cesnet.cz>
2024-10-12 23:15:13 +0200

    01_module

--- src/ldpymod.c
+++ src/ldpymod.c
@@ -1,38 +1,12 @@
 #include <Python.h>
-#include <stddef.h>
-#include "ldpymod.h"
-#include "object.h"
-
-PyObject *GeneralError;
-PyObject *SpecificError;
-
-PyObject *
-ldpymod_hello(PyObject *self)
-{
-    const char *string = "Hello world!";
-    int number = 1234;
-
-    // https://docs.python.org/3/c-api/arg.html#c.Py_BuildValue
-    return Py_BuildValue("(si)", string, number);
-}
-
-// https://docs.python.org/3/c-api/structures.html#c.PyMethodDef
-static PyMethodDef ldpymod_methods[] = {
-    {"hello", (PyCFunction) ldpymod_hello, METH_NOARGS,
-     "Get tuple with string and number.\n\n"
-     "Returns:\n"
-     "    tuple(str, int)\n\n"},
-    {NULL, NULL, 0, NULL}
-};
 
 // https://docs.python.org/3/c-api/module.html#c.PyModuleDef
 static struct PyModuleDef ldpymodmodule = {
     PyModuleDef_HEAD_INIT,
     "ldpymod",   /* name of module */
     "This is the documentation of this module.\n",
-    -1,   /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-    ldpymod_methods,
-    NULL, NULL, NULL, NULL
+    -1,   /* size of per-interpreter state of the module, -1 = global variables. */
+    NULL, NULL, NULL, NULL, NULL
 };
 
 PyMODINIT_FUNC
@@ -45,25 +19,5 @@ PyInit_ldpymod(void)
         return NULL;
     }
 
-    if (PyType_Ready(&LinuxDaysObj) < 0) {
-        return NULL;
-    }
-    Py_INCREF(&LinuxDaysObj);
-    PyModule_AddObject(m, "LinuxDaysObj", (PyObject *) &LinuxDaysObj);
-
-    /* Add Exceptions into pytrap module */
-    GeneralError = PyErr_NewException("ldpymod.GeneralError", NULL, NULL);
-    Py_INCREF(GeneralError);
-    PyModule_AddObject(m, "GeneralError", GeneralError);
-
-    SpecificError = PyErr_NewException("ldpymod.SpecificError", GeneralError, NULL);
-    Py_INCREF(SpecificError);
-    PyModule_AddObject(m, "SpecificError", SpecificError);
-
-    /* Add constants into pytrap module */
-    PyModule_AddIntConstant(m, "FMT_RAW", 1);
-    PyModule_AddIntConstant(m, "FMT_JSON", 2);
-
     return m;
-
 }
--- tests/test_ldpymod.py
+++ tests/test_ldpymod.py
@@ -9,20 +9,3 @@ class ldmodule_tests(unittest.TestCase):
         except ImportError as e:
             self.fail(str(e))
 
-        self.assertEqual(ldpymod.hello(), ("Hello world!", 1234))
-
-class ldobject_tests(unittest.TestCase):
-    def runTest(self):
-        import ldpymod
-
-        o = ldpymod.LinuxDaysObj()
-        with self.assertRaises(TypeError):
-            o.area("abc")
-        with self.assertRaises(TypeError):
-            o.area(["2, 2, 3"])
-
-        self.assertEqual(o.area([(2, 2, 3)]), 1.984313483298443)
-        self.assertEqual(o.area([(3, 2, 4)]), 2.9047375096555625)
-        self.assertEqual(o.area([(2, 2, 3), (3, 2, 4)]), 4.889050992954005)
-        self.assertEqual(o.area([(2, 2, 3), (3, 2, 4)], 10, 10, 10), 48890.50992954006)
-
